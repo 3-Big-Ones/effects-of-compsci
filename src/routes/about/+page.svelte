d7177e61c2d139706379a3c6cccfa5f4a6dc7ef9
Alexandre Dias <34582078+DerpDays@users.noreply.github.com>
2023-11-03 15:03:22 +0000

    Dark mode

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col min-w-full justify-center items-center">
-    <h1 class="text-3xl mt-40">About</h1>
-    <p>This was built by Alexandre, Keima, and An. It showcase how computer science is used in society.</p>
+<div class="flex flex-col grow justify-center items-center dark:bg-zinc-800">
+    <h1 class="text-3xl dark:text-white mt-40">About</h1>
+    <p class="dark:text-white p-8">This was built by Alexandre, Keima, and An. It showcase how computer science is used in society.</p>
 </div>
